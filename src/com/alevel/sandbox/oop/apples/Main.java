892d640ce4584184b72c276d3ff7bbf7178003b9
voidaspect <miwagorbi@gmail.com>
2018-07-10 19:46:24 +0300

    downcast/instanceof

--- src/com/alevel/sandbox/oop/apples/Main.java
+++ src/com/alevel/sandbox/oop/apples/Main.java
@@ -1,4 +1,31 @@
 package com.alevel.sandbox.oop.apples;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Apple[] apples = {
+                new Apple(),
+                new Apple(),
+                new RottenApple(1),
+                new Apple(),
+                new RottenApple(2),
+                new Apple()
+        };
+
+        findRottenApples(apples);
+    }
+
+    private static void findRottenApples(Apple... apples) {
+        for (int i = 0; i < apples.length; i++) {
+            Apple apple = apples[i];
+            if (apple instanceof RottenApple) {
+                RottenApple ra = (RottenApple) apple;
+                System.out.println("Apple " + i + " is rotten, it has " +
+                        ra.getWormCount() + " worms in it!");
+            }
+        }
+    }
+
+
 }
--- src/com/alevel/sandbox/oop/apples/RottenApple.java
+++ src/com/alevel/sandbox/oop/apples/RottenApple.java
@@ -1,4 +1,18 @@
 package com.alevel.sandbox.oop.apples;
 
-public class RottenApple {
+public class RottenApple extends Apple {
+
+    private int wormCount;
+
+    public RottenApple(int wormCount) {
+        this.wormCount = wormCount;
+    }
+
+    public int getWormCount() {
+        return wormCount;
+    }
+
+    public void setWormCount(int wormCount) {
+        this.wormCount = wormCount;
+    }
 }
