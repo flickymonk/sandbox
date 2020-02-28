f34c4fbeb48c4b24a9dec246304789c235e57c34
Mikhail Horbunov <miwagorbi@gmail.com>
2020-02-28 21:55:44 +0200

    stream api examples

---
+++ core/src/main/java/com/alevel/sandbox/stream/Assignment.java
@@ -0,0 +1,93 @@
+package com.alevel.sandbox.stream;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.*;
+import java.util.function.Function;
+
+import static java.util.stream.Collectors.*;
+
+public final class Assignment {
+
+    public static final class ParseDecimalNumberFromString {
+
+        public static void main(String[] args) {
+            List<String> strings = Arrays.asList("string 1 text", "2 string 3 text", "45");
+            System.out.println(strings + ": " + parse(strings));
+        }
+
+        public static long parse(Collection<String> strings) {
+
+            return strings.stream()
+                    .flatMapToInt(String::codePoints)
+                    .filter(Character::isDigit)
+                    .map(codepoint -> Character.digit(codepoint, 10))
+                    .asLongStream()
+                    .reduce((result, next) -> result * 10 + next)
+                    .orElseThrow(() -> new IllegalArgumentException("No digits found in " + strings));
+        }
+
+    }
+
+    public static final class MostCommonNumber {
+
+        public static void main(String[] args) {
+            System.out.println(find(0, 1, 34, 1, 0, 4, 0, -324, 1000));
+            System.out.println(find(0, 1, 34, 1, 0, 4, 34, 34, -324, 1000));
+            System.out.println(find(-1));
+            System.out.println(find(3, 3, 3, -3, -3, -3));
+            System.out.println(find(-3, 3, 3, 3, -3, -3));
+            System.out.println(find(2, 2, -3, 3, 3, 3, -3, -3, -3));
+        }
+
+        public static int find(int... numbers) {
+
+            Map<Integer, Integer> counts = Arrays.stream(numbers).boxed()
+                    .collect(toMap(Function.identity(), occurrence -> 1, Integer::sum, LinkedHashMap::new));
+
+            return counts.entrySet().stream()
+                    .max(Comparator.comparingInt(Map.Entry::getValue))
+                    .map(Map.Entry::getKey)
+                    .orElseThrow(() -> new IllegalArgumentException(
+                            "Could not find the most commonly occurring element in an empty array"));
+        }
+
+    }
+
+    public static final class GroupTimeByDates {
+
+        public static void main(String[] args) {
+            LocalDateTime now = LocalDateTime.now();
+            List<LocalDateTime> timestamps = Arrays.asList(
+                    now,
+                    now.minusHours(1),
+                    now.plusHours(1),
+                    now.minusDays(1),
+                    now.plusDays(1),
+                    now.plusHours(25),
+                    now.plusDays(2)
+            );
+            System.out.println(timestamps);
+
+            SortedMap<LocalDate, List<LocalTime>> byDate = groupByDateAscending(timestamps);
+            System.out.println(byDate);
+
+            String newline = System.lineSeparator();
+            String humanReadableOutput = byDate.entrySet().stream()
+                    .map(String::valueOf)
+                    .collect(joining(newline, "Human readable output:" + newline, newline));
+            System.out.print(humanReadableOutput);
+        }
+
+        public static SortedMap<LocalDate, List<LocalTime>> groupByDateAscending(Collection<LocalDateTime> timestamps) {
+
+            return timestamps.stream().collect(groupingBy(
+                    LocalDateTime::toLocalDate,
+                    TreeMap::new,
+                    mapping(LocalDateTime::toLocalTime, toList())
+            ));
+        }
+    }
+
+}
---
+++ core/src/main/java/com/alevel/sandbox/stream/StreamAPIDemo.java
@@ -0,0 +1,114 @@
+package com.alevel.sandbox.stream;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.*;
+
+public final class StreamAPIDemo {
+
+    public static final class FindNamesWithSpaces {
+
+        public static void main(String[] args) {
+            List<String> names = List.of("Irene", "Zhao Liang", "Romulus", "Richard Matthew");
+
+            //region solution
+            List<String> withSpaces = names.stream()
+                    .filter(name -> name.contains(" "))
+                    .collect(toList());
+            //endregion
+
+            System.out.println(withSpaces);
+        }
+
+    }
+
+    public static final class GroupStringsByLength {
+
+        public static void main(String[] args) {
+            List<String> strings = List.of("xxx", "xx", "x", "yyy", "zzz");
+
+            //region solution
+            Map<Integer, List<String>> groupByLength = strings.stream().collect(groupingBy(String::length));
+            //endregion
+
+            System.out.println(groupByLength);
+        }
+    }
+
+    public static final class FindPositiveAndNegativeNotEqualToZero {
+
+        public static void main(String[] args) {
+            int[] numbers = {2, -5, 0, 124, 32, -1432, 432, 566, -1, 1, 10, 2, 0, -1};
+
+            //region solution
+            Map<Boolean, Set<Integer>> positiveAndNegative = Arrays.stream(numbers)
+                    .filter(num -> num != 0)
+                    .boxed()
+                    .collect(partitioningBy(num -> num > 0, toSet()));
+            //endregion
+
+            System.out.println("Positive: " + positiveAndNegative.get(true));
+            System.out.println("Negative: " + positiveAndNegative.get(false));
+        }
+
+    }
+
+    public static final class FindPositiveAndNegativeNotEqualToZeroSortByAbsoluteValue {
+
+        public static void main(String[] args) {
+            int[] numbers = {2, -5, 0, 124, 32, -1432, 432, 566, -1, 1, 10, 2, 0, -1};
+
+            //region solution
+            Map<Boolean, Set<Integer>> positiveAndNegative = Arrays.stream(numbers)
+                    .filter(num -> num != 0)
+                    .boxed()
+                    .collect(partitioningBy(
+                            num -> num > 0,
+                            toCollection(() -> new TreeSet<>(Comparator.comparingInt(Math::abs)))
+                    ));
+            //endregion
+
+            System.out.println("Positive: " + positiveAndNegative.get(true));
+            System.out.println("Negative: " + positiveAndNegative.get(false));
+        }
+
+    }
+
+    public static final class GenerateRandomUUIDs {
+
+        public static Stream<UUID> generate(int limit) {
+            return Stream.generate(UUID::randomUUID).limit(limit);
+        }
+
+        public static void main(String[] args) {
+            String output = generate(20)
+                    .map(UUID::toString)
+                    .collect(joining(System.lineSeparator()));
+            System.out.println(output);
+        }
+
+    }
+
+    public static final class CalculateHashCodeInParallel {
+
+        public static int hashcode(Collection<?> objects) {
+            return objects.parallelStream()
+                    .mapToInt(Objects::hashCode)
+                    .reduce(1, (hash, next) -> 31 * hash + next);
+        }
+
+        public static void main(String[] args) {
+            List<UUID> objects = GenerateRandomUUIDs.generate(5_000_000).collect(toList());
+            System.out.println(hashcode(objects));
+            System.out.println(hashcode(objects));
+            System.out.println(hashcode(objects));
+            System.out.println(hashcode(objects));
+        }
+
+    }
+}
