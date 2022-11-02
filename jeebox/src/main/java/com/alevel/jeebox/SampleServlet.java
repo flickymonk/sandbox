b2a3c6ffec6055b31f047886deeeb33bcfa5729e
Mykhailo Horbunov <mykhailo.horbunov@auto1.com>
2022-11-02 20:43:07 +0200

    jeebox updated to jdk 17

--- jeebox/src/main/java/com/alevel/jeebox/SampleServlet.java
+++ jeebox/src/main/java/com/alevel/jeebox/SampleServlet.java
@@ -9,11 +9,13 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.Serial;
 
 @WebServlet(name = "sample-servlet", urlPatterns = "/sample")
 public class SampleServlet extends HttpServlet {
 
-    private static final long serialVersionUID = -8948379822734246956L;
+    @Serial
+    private static final long serialVersionUID = -1738342798982908762L;
 
     private static final Logger log = LoggerFactory.getLogger(SampleServlet.class);
 
@@ -27,15 +29,18 @@ public class SampleServlet extends HttpServlet {
         PrintWriter responseBody = resp.getWriter();
 
         resp.setContentType("text/html");
-        responseBody.println("<h1 align=\"center\">Sample Servlet GET method processing</h1>");
-        responseBody.println("<h3 align=\"center\">Request from: " + req.getRemoteHost() + "</h3>");
+
+        responseBody.printf("""
+                <h1 align="center">Sample Servlet GET method processing</h1>
+                <h3 align="center">Request from: %s</h3>%n
+                """, req.getRemoteHost());
 
         String client = req.getParameter("client");
         if (client == null) {
             client = "anonymous user";
         }
 
-        responseBody.println("<h3 align=\"center\">Hi, " + client + " </h3>");
+        responseBody.printf("<h3 align=\"center\">Hi, %s</h3>%n", client);
     }
 
     @Override
